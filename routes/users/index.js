92cc5b410452f0a9a6bc247fde22ec0c46d149be
Nick <nick@panagakis.com>
2019-12-17 20:03:38 -0600

    Added important files

---
+++ routes/users/index.js
@@ -0,0 +1,35 @@
+const Router = require("express").Router;
+const db = require("../../models");
+
+const userRoutes = Router();
+
+userRoutes
+  .route("/")
+
+  .get(async (req, res) => {
+    const Users = await db.User.findAll({
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+    res.json(Users);
+  })
+
+  // Registration
+  .post(async (req, res) => {
+    const dbUser = await db.User.create(req.body);
+    res.json(dbUser);
+  });
+
+// Delete a user by id
+// This should require authentication
+userRoutes.delete("/:id", async (req, res) => {
+  const dbUser = await db.User.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.json(dbUser);
+});
+
+module.exports = userRoutes;
