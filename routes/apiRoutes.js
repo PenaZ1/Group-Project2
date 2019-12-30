90dc6d663a28edb99332e405376bda270819112f
4stru <4.struong@kscholars.org>
2019-12-29 19:09:11 -0600

    added into api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,12 @@
 const Router = require("express").Router;
 const userRoutes = require("./users");
 const postRoutes = require("./posts");
+const likeRoutes = require("./likes");
 
 const apiRoutes = Router();
 
 apiRoutes.use("/users", userRoutes);
 apiRoutes.use("/posts", postRoutes);
+apiRoutes.use("/likes", likeRoutes);
 
 module.exports = apiRoutes;
